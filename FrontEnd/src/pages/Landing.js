9326fc08d27a9f53e20a144268cc3962ec9cb40c
Raqib-Yoon <raqibyoon2020@gmail.com>
2023-09-03 19:02:54 -0700

    comment added to landing page
    i added this comment to the landing page

--- FrontEnd/src/pages/Landing.js
+++ FrontEnd/src/pages/Landing.js
@@ -3,6 +3,7 @@ import { Typography } from "@mui/material";
 const Landing = () => {
   return (
     <>
+    {/* this is the landing page */}
       <Typography variant='h3' color='primary.main'>This is Landing Page With Primary Color</Typography>
     </>
   );
