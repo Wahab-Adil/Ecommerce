eeb9029a3363a21eac44b7c565ab7b8769c845d5
Wahab Adil <wahab.cs238@gmail.com>
2023-09-28 12:56:02 +0430

    DB-connection setup done

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -1,3 +1,13 @@
-import express from 'express';
-const app  = express();
+import express from "express";
+import dotenv from "dotenv";
+import dbConnection from "../config/dbConnect.js";
+
+// env config
+dotenv.config();
+
+// calling DB Connection Function
+dbConnection();
+
+// calling express
+const app = express();
 export default app;
