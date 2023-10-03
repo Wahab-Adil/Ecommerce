b50a3a0503ce72bc8a2e96694a2acfb89e38329e
Wahab Adil <wahab.cs238@gmail.com>
2023-10-03 22:33:03 +0430

    filter Product done

--- Backend/controllers/productCtr.js
+++ Backend/controllers/productCtr.js
@@ -52,3 +52,62 @@ export const getAllProducts = expressAsyncHandler(async (req, res, next) => {
     throw new Error("No Products Avaliable");
   }
 });
+
+//  logic For  filtering  Products
+
+// @-desc-  filter product
+// @route - api/product/ ?value
+// @access  Public
+
+export const filterProduct = expressAsyncHandler(async (req, res, next) => {
+  let filterdProducts = ProductModel.find();
+
+  // filtering products by product name
+  if (req.query.name) {
+    filterdProducts = filterdProducts.find({
+      name: { $regex: new RegExp(req.query.name, "i") },
+    });
+  }
+  // filtering products by color
+  if (req.query.color) {
+    filterdProducts = filterdProducts.find({
+      colors: { $regex: new RegExp(req.query.color, "i") },
+    });
+  }
+  // filtering products by brand
+  if (req.query.brand) {
+    filterdProducts = filterdProducts.find({
+      brand: { $regex: new RegExp(req.query.brand, "i") },
+    });
+  }
+  // filtering products by size
+  if (req.query.size) {
+    filterdProducts = filterdProducts.find({
+      size: { $regex: new RegExp(req.query.brand, "i") },
+    });
+  }
+  // filtering products by category
+  if (req.query.category) {
+    filterdProducts = filterdProducts.find({
+      category: { $regex: new RegExp(req.query.category, "i") },
+    });
+  }
+  // filtering product by price
+  if (req.query.price) {
+    // spliting price range
+    const priceRange = req.query.price.split("-");
+    // less then and equal
+    const lte = priceRange[0];
+    // greater then and equal
+    const gte = priceRange[1];
+    // check if product equal to price range user Mentioned
+    filterdProducts = filterdProducts.find({
+      price: { $gte: lte, $lte: gte },
+    });
+  }
+
+  // final result of filterd product
+  const products = await filterdProducts;
+
+  res.json({ status: "success", filterProducts: products });
+});
--- Backend/routes/productsRoute.js
+++ Backend/routes/productsRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createProductCtr, getAllProducts } from "../controllers/productCtr.js";
+import {
+  createProductCtr,
+  filterProduct,
+  getAllProducts,
+} from "../controllers/productCtr.js";
 import isLoggedIn from "../middlewares/isloggedIn.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/create", isLoggedIn, createProductCtr);
 productRouter.get("/all", getAllProducts);
+productRouter.get("/", filterProduct);
 
 export default productRouter;
