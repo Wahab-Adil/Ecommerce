426ed59b4443c2f7b21c1a94c754655425254487
Wahab Adil <wahab.cs238@gmail.com>
2023-10-05 17:58:25 +0430

    fetch all categories ctr & route created

--- Backend/controllers/categroyCtr.js
+++ Backend/controllers/categroyCtr.js
@@ -1,6 +1,11 @@
 import expressAsyncHandler from "express-async-handler";
 import categoryModel from "../models/category.js";
 
+//  logic For  create category
+
+// @-desc-  creating category
+// @route - api/category/create
+// @access  Private/Admin
 export const createCategory = expressAsyncHandler(async (req, res) => {
   const { name, image, products } = req.body;
 
@@ -22,4 +27,18 @@ export const createCategory = expressAsyncHandler(async (req, res) => {
     message: "Category succefully Created",
     category: createdCategory,
   });
+});
+
+//  logic For  fetch all category
+
+// @-desc-  fetch categories
+// @route - api/category/
+// @access  Public
+
+export const getAllCategories = expressAsyncHandler(async (req, res) => {
+  const categories = await categoryModel.find({});
+  if (!categories) {
+    throw new Error("Categories not found");
+  }
+  res.json({ status: "success", categories });
 });
