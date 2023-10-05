7f1493afbfc3154a26fa8ee25813238831b5afb4
Wahab Adil <wahab.cs238@gmail.com>
2023-10-05 18:26:52 +0430

    fetch single product created

--- Backend/routes/categroyRoute.js
+++ Backend/routes/categroyRoute.js
@@ -3,11 +3,13 @@ import isLoggedIn from "../middlewares/isloggedIn.js";
 import {
   createCategory,
   getAllCategories,
+  getSingleCategory,
 } from "../controllers/categroyCtr.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/create", isLoggedIn, createCategory);
-categoryRouter.post("/", isLoggedIn, getAllCategories);
+categoryRouter.get("/", isLoggedIn, getAllCategories);
+categoryRouter.get("/:id", isLoggedIn, getSingleCategory);
 
 export default categoryRouter;
