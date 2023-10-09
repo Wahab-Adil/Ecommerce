ee5507de5ab204bd12a75fbd96827fba71e0062e
Wahab Adil <wahab.cs238@gmail.com>
2023-10-09 15:25:39 +0430

    Color Model Created !

---
+++ Backend/models/color.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const ColorSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  products: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "product",
+    },
+  ],
+});
+
+const ColorShema = mongoose.model("Brand", ColorSchema);
+export default ColorShema;
