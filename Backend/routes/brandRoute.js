c33bc081bf47733399a548fb047e33ba9ded371f
Wahab Adil <wahab.cs238@gmail.com>
2023-10-09 14:27:12 +0430

    fetch SIngle Brand Ctr & route created

--- Backend/routes/brandRoute.js
+++ Backend/routes/brandRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import isLoggedIn from "../middlewares/isloggedIn.js";
-import { createBrand, getAllBrands } from "../controllers/brandCtr.js";
+import {
+  createBrand,
+  getAllBrands,
+  getSingleBrand,
+} from "../controllers/brandCtr.js";
 
 const brandRouter = express.Router();
 
 brandRouter.post("/create", isLoggedIn, createBrand);
 brandRouter.get("/", getAllBrands);
+brandRouter.get("/:id", getSingleBrand);
 
 export default brandRouter;
