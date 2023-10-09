ec5ca3e78e5f716176421e17f2efe1c4343bdd4b
Wahab Adil <wahab.cs238@gmail.com>
2023-10-09 14:16:54 +0430

    route for getAllBrands created

--- Backend/routes/brandRoute.js
+++ Backend/routes/brandRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import isLoggedIn from "../middlewares/isloggedIn.js";
-import { createBrand } from "../controllers/brandCtr.js";
+import { createBrand, getAllBrands } from "../controllers/brandCtr.js";
 
 const brandRouter = express.Router();
 
 brandRouter.post("/create", isLoggedIn, createBrand);
+brandRouter.get("/", getAllBrands);
 
 export default brandRouter;
