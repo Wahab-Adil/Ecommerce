88659008e97fe8baef0fe8c665260121c3272779
Wahab Adil <wahab.cs238@gmail.com>
2023-11-02 18:25:51 +0430

    route added for update Controller

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -5,11 +5,13 @@ import {
   createOrder,
   fetchAllOrders,
   fetchSingleOrder,
+  updateOrderCtr,
 } from "../controllers/orderCtr.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/create", isLoggedIn, createOrder);
+orderRouter.put("/update/:id", isLoggedIn, updateOrderCtr);
 orderRouter.get("/", isLoggedIn, fetchAllOrders);
 orderRouter.get("/:id", fetchSingleOrder);
 
