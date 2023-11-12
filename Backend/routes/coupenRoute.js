0f973925905ac175b55df3f7b30b25ce5bca1fb2
Wahab Adil <wahab.cs238@gmail.com>
2023-11-12 20:05:18 +0430

    route added for all coupen Ctr

--- Backend/routes/coupenRoute.js
+++ Backend/routes/coupenRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import isLoggedIn from "../middlewares/isloggedIn.js";
-import { createCoupen } from "../controllers/coupenCtr.js";
+import { createCoupen, getAllCoupons } from "../controllers/coupenCtr.js";
 
 const brandRouter = express.Router();
 
 brandRouter.post("/create", isLoggedIn, createCoupen);
+brandRouter.get("/", isLoggedIn, getAllCoupons);
 
 export default brandRouter;
