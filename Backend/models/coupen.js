08a67f239c4fbe588683c6d1bf938a1312b224de
Wahab Adil <wahab.cs238@gmail.com>
2023-11-12 19:36:48 +0430

    coupon validation added

--- Backend/models/coupen.js
+++ Backend/models/coupen.js
@@ -32,6 +32,37 @@ const CouponSchema = new Schema(
     toJSON: { virtuals: true },
   }
 );
+CouponSchema.virtual("isExpired").get(function () {
+  return this.endDate < Date.now();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.endDate < this.startDate) {
+    next(new Error("End Date can not less then Start Date"));
+  }
+  next();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.discount <= 0 || this.discount >= 100) {
+    next(new Error("discount can not be less then 0 or greater then 100"));
+  }
+  next();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.startDate < Date.now()) {
+    next(new Error("Start date can not be less then today"));
+  }
+  next();
+});
+
+CouponSchema.pre("validate", function (next) {
+  if (this.endDate < Date.now()) {
+    next(new Error("End date can not be less then today"));
+  }
+  next();
+});
 
 const coupenModel = mongoose.model("Coupon", CouponSchema);
 
