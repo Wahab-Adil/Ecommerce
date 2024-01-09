6b0bc52649ce4956d46529df76a42312ecf41fb0
Wahab Adil <wahab.cs238@gmail.com>
2024-01-09 22:29:10 +0430

    ui animation edit day1

---
+++ FrontEnd/src/utils/textTransaction.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TextTransition, { presets } from "react-text-transition";
+
+const TextTransaction = ({ TEXTS, duration, color }) => {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      duration // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
+  return (
+    <h1>
+      <TextTransition style={{ color }} springConfig={presets.wobbly}>
+        {TEXTS[index % TEXTS.length]}
+      </TextTransition>
+    </h1>
+  );
+};
+export default TextTransaction;
